597a98c1979e8918cb540cf165aff3a774ed8626
garciaalan186 <67207869+garciaalan186@users.noreply.github.com>
2020-08-24 12:42:01 -0500

    Add init and every_waking_hour method.

---
+++ kids.py
@@ -0,0 +1,10 @@
+class Kids:
+    def __init__(self, name, parent):
+        self.name = name
+        self.parent = parent
+
+    def every_waking_moment(self):
+        do while self.parent.attention(elsewhere):
+            self.parent.attention(self)
+
+
